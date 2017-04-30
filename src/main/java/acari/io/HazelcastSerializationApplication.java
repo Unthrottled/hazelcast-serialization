a2972a76370458c6857c0fadc3ae0e175c30ed78
cyclic-reference <alex@cyclicreference.space>
2017-04-30 16:49:59 -0500

    Intial Commit

---
+++ src/main/java/acari/io/HazelcastSerializationApplication.java
@@ -0,0 +1,12 @@
+package acari.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastSerializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastSerializationApplication.class, args);
+	}
+}
