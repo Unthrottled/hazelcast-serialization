92b9d56a6ddcfd1e98ba0d0ed0a9d49c7759c58b
cyclic-reference <alex@cyclicreference.space>
2017-04-30 19:58:56 -0500

    Many Programmers!

---
+++ src/main/java/acari/io/LanguageRepository.java
@@ -0,0 +1,21 @@
+package acari.io;
+
+import com.google.common.collect.Lists;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Component
+public class LanguageRepository {
+    private final List<String> LANGUAGES = Lists.newLinkedList(
+            Lists.newArrayList("Java", "Groovy", "Go", "Clojure", "Lisp", "Ruby",
+                    "Javascript", "C++", "C","C#", "Perl","Cobol","R", "Matlab"));
+    private final Random ranbo = new Random(9002);
+
+    List<String> randomLanguages(){
+        return LANGUAGES.stream().filter(s->ranbo.nextBoolean()).collect(Collectors.toList());
+    }
+
+}
--- src/main/java/acari/io/ProgrammerRepostitory.java
+++ src/main/java/acari/io/ProgrammerRepostitory.java
@@ -1,16 +1,18 @@
 package acari.io;
 
 import acari.io.pojo.Programmer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.math.BigInteger;
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -20,16 +22,19 @@ import java.util.stream.Stream;
 
 @Component
 public class ProgrammerRepostitory {
+    private static final Logger logger = LoggerFactory.getLogger(ProgrammerRepostitory.class);
     private static final int THREADS = 16;
     private final SecureRandom secureRandom = new SecureRandom();
     private final Random ranbo = new Random(9001);
     private final ComputerRepository computerRepository;
-    private Collection<Programmer> programmerCollection;
+    private final LanguageRepository languageRepository;
+    private final LinkedList<Programmer> programmerCollection = new LinkedList<>();
     private ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
 
     @Autowired
-    public ProgrammerRepostitory(ComputerRepository computerRepository) {
+    public ProgrammerRepostitory(ComputerRepository computerRepository, LanguageRepository languageRepository) {
         this.computerRepository = computerRepository;
+        this.languageRepository = languageRepository;
     }
 
     @PostConstruct
@@ -40,8 +45,19 @@ public class ProgrammerRepostitory {
                 .map(i -> executorService.submit(() -> Stream.generate(integerSupplier)
                         .limit(100000)
                         .map(j -> new Programmer(new BigInteger(2048, secureRandom).toString(32),
-                                ranbo.nextInt(64), computerRepository.randomComputer(), new ArrayList<>()))
-                        .collect(Collectors.toList()))).collect(Collectors.toList());
+                                ranbo.nextInt(64), computerRepository.randomComputer(), languageRepository.randomLanguages()))
+                        .collect(Collectors.toList()))).collect(Collectors.toCollection(LinkedList::new));
+        for (Future<List<Programmer>> futureProgrammer : futureProgrammers) {
+            try {
+                programmerCollection.addAll(futureProgrammer.get());
+            } catch (InterruptedException | ExecutionException e) {
+                logger.warn("Problem creating programmers.", e);
+            }
+        }
+    }
+
+    public Stream<Programmer> getProgrammers(){
+        return programmerCollection.parallelStream();
     }
 
 }
